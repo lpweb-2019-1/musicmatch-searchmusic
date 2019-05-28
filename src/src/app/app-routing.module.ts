5622065771b0ece5ff4ff1c4deae011e415eddb1
lucaslira757 <lucaslira757@gmail.com>
2019-05-27 22:02:34 -0300

    Add files via upload

---
+++ src/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MusicaComponent } from './musica/musica.component';
+import { MusicasComponent } from './musicas/musicas.component';
+import { ArtistaComponent } from './artista/artista.component';
+
+const routes: Routes = [
+  {path: 'artistas/:id', component: ArtistaComponent},
+  { path: 'musicas', component: MusicasComponent },
+  { path: 'musicas/:id', component: MusicaComponent },
+  { path: '', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
